42a91c80e2e2507e595484bf22e3a946d2c38d06
Riccardo Lancellotti <riccardo.lancellotti@unimore.it>
2021-04-27 23:35:17 +0200

    Removed unwanted comments

--- src/FogSource.h
+++ src/FogSource.h
@@ -13,8 +13,8 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
-#ifndef __CLOUDAPPSOURCE_H
-#define __CLOUDAPPSOURCE_H
+#ifndef __FOGSOURCE_H
+#define __FOGSOURCE_H
 
 #include <omnetpp.h>
 using namespace omnetpp;
