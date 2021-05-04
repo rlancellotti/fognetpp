cd0d7ff5d9b812f7f343efc346392abd941a0321
Riccardo Lancellotti <riccardo.lancellotti@unimore.it>
2021-05-04 12:19:50 +0200

    New VariableDelayChannel implementation

---
+++ src/VariableDelayChannel.cc
@@ -0,0 +1,69 @@
+
+#include "VariableDelayChannel.h"
+#include "omnetpp/cmessage.h"
+#include "omnetpp/cenvir.h"
+#include "omnetpp/globals.h"
+#include "omnetpp/cgate.h"
+#include "omnetpp/cexception.h"
+#include "omnetpp/ctimestampedvalue.h"
+
+namespace fog {
+
+Register_Class(VariableDelayChannel);
+
+simsignal_t VariableDelayChannel::messageSentSignal;
+simsignal_t VariableDelayChannel::messageDiscardedSignal;
+
+VariableDelayChannel *VariableDelayChannel::create(const char *name)
+{
+    return dynamic_cast<VariableDelayChannel *>(cChannelType::getDelayChannelType()->create(name));
+}
+
+void VariableDelayChannel::initialize()
+{
+    messageSentSignal = registerSignal("messageSent");
+    messageDiscardedSignal = registerSignal("messageDiscarded");
+}
+
+void VariableDelayChannel::rereadPars()
+{
+    setFlag(FL_ISDISABLED, par("disabled"));
+}
+
+void VariableDelayChannel::handleParameterChange(const char *)
+{
+    rereadPars();
+}
+
+void VariableDelayChannel::setDelay(double d)
+{
+    par("delay").setDoubleValue(d);
+}
+
+void VariableDelayChannel::setDisabled(bool d)
+{
+    par("disabled").setBoolValue(d);
+}
+
+void VariableDelayChannel::processMessage(cMessage *msg, simtime_t t, result_t& result)
+{
+    // if channel is disabled, signal that message should be deleted
+    if (flags & FL_ISDISABLED) {
+        result.discard = true;
+        cTimestampedValue tmp(t, msg);
+        emit(messageDiscardedSignal, &tmp);
+        return;
+    }
+
+    // propagation delay modeling
+    result.delay = par("delay");
+
+    // emit signals
+    if (mayHaveListeners(messageSentSignal)) {
+        MessageSentSignalValue tmp(t, msg, &result);
+        emit(messageSentSignal, &tmp);
+    }
+}
+
+}  // namespace fog
+
---
+++ src/VariableDelayChannel.h
@@ -0,0 +1,140 @@
+#ifndef __VARIABLEDELAYCHANNEL_H
+#define __VARIABLEDELAYCHANNEL_H
+
+#include "omnetpp/cchannel.h"
+
+using namespace omnetpp;
+
+namespace fog {
+
+/**
+ * @brief Channel with variable propagation delay.
+ *
+ */
+class VariableDelayChannel : public cChannel //implies noncopyable
+{
+  protected:
+    static simsignal_t messageSentSignal;
+    static simsignal_t messageDiscardedSignal;
+
+  private:
+    enum {
+      FL_ISDISABLED = 1 << 10,
+      FL_DELAY_NONZERO = 1 << 11,
+    };
+
+  private:
+    // internal: checks whether parameters have been set up
+    void checkState() const  {if (!parametersFinalized()) throw cRuntimeError(this, E_PARAMSNOTREADY);}
+
+  protected:
+    // internal: update cached copies of parameters
+    void rereadPars();
+
+    /**
+     * Called back when a parameter changes. Redefined from cComponent.
+     */
+    virtual void handleParameterChange(const char *parname) override;
+
+  public:
+    /** @name Constructors, destructor */
+    //@{
+    /**
+     * Constructor. This is only for internal purposes, and should not
+     * be used when creating channels dynamically; use the create()
+     * factory method instead.
+     */
+    explicit VariableDelayChannel(const char *name=nullptr) : cChannel(name) {}
+
+    /**
+     * Destructor.
+     */
+    virtual ~VariableDelayChannel() {}
+
+    /**
+     * Utility function for dynamic channel creation. Equivalent to
+     * <tt>cChannelType::getDelayChannelType()->create(name)</tt>.
+     */
+    static VariableDelayChannel *create(const char *name);
+    //@}
+
+    /** @name Redefined cChannel member functions. */
+    //@{
+    /**
+     * The VariableDelayChannel implementation of this method always returns false.
+     */
+    virtual bool isTransmissionChannel() const override {return false;}
+
+    /**
+     * The VariableDelayChannel implementation of this method always returns zero.
+     */
+    virtual double getNominalDatarate() const override {return 0;}
+
+    /**
+     * The VariableDelayChannel implementation of this method always returns zero.
+     */
+    virtual simtime_t calculateDuration(cMessage *msg) const override {return 0;}
+
+    /**
+     * The VariableDelayChannel implementation of this method always returns zero.
+     */
+    virtual simtime_t getTransmissionFinishTime() const override {return SIMTIME_ZERO;}
+
+    /**
+     * The VariableDelayChannel implementation of this method always returns false.
+     */
+    virtual bool isBusy() const override {return false;}
+
+    /**
+     * The VariableDelayChannel implementation of this method does nothing.
+     */
+    virtual void forceTransmissionFinishTime(simtime_t t) override {}
+    //@}
+
+    /** @name Setting and getting channel parameters. */
+    //@{
+    /**
+     * Sets the propagation delay of the channel, in seconds.
+     */
+    virtual void setDelay(double d);
+
+    /**
+     * Disables or enables the channel.
+     */
+    virtual void setDisabled(bool d);
+
+    /**
+     * Returns the propagation delay of the channel, in seconds.
+     * This method is equivalent to reading the "delay" parameter, via par("delay").
+     */
+    virtual simtime_t getDelay() const {checkState(); return (simtime_t) par("delay");}
+
+    /**
+     * Returns whether the channel is disabled. This method is equivalent
+     * to reading the "disabled" parameter, via par("disabled").
+     * A disabled channel discards all messages sent on it.
+     */
+    virtual bool isDisabled() const  {checkState(); return flags & FL_ISDISABLED;}
+    //@}
+
+    /** @name Implementation methods */
+    //@{
+    /**
+     * Initialization.
+     */
+    virtual void initialize() override;
+
+    /**
+     * This implementation delivers the message to the opposite gate
+     * with a delay.
+     */
+    virtual void processMessage(cMessage *msg, simtime_t t, result_t& result) override;
+    //@}
+};
+
+}  // namespace fog
+
+
+#endif
+
+
